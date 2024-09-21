6884c5cc55d385c0f91a5924c7f2958871cf9f7b
Alex Chao <chaoalex93@gmail.com>
2024-09-20 17:13:00 -0700

    added the Revenue chart component with its visual

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,9 @@ import { fetchRevenue } from '@/app/lib/data';
  
 export default async function Page() {
   const revenue = await fetchRevenue();
+
+  console.log('Revenue data:', revenue);
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
